60b315b665acec57e51134009b3bc9c8c477b9ad
Mae Duphorne <maelduphorne@gmail.com>
2021-11-08 17:42:00 -0700

    fix fetch calls and imports

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import apiCalls from '../api/apiCalls';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [stories, setStories]= useState([])
+  const [error, setError]= useState('')
+
+  useEffect(() => {
+    setError('')
+    apiCalls.fetchStoriesData()
+    .then((data => setStories(data.results)))
+    .catch((err) => console.log(err))
+  })
+
+  return (
+    <div className="App">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
