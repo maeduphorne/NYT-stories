1dff613e9c025ffbdbcebc92bec95c14243c8148
Mae Duphorne <maelduphorne@gmail.com>
2021-11-08 18:33:17 -0700

    import story cards area for display

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,24 @@
 import './App.css';
 import apiCalls from '../api/apiCalls';
 import Header from '../Header/Header';
+import StoryCardsArea from '../StoryCardsArea/StoryCardsArea';
 import { useEffect, useState } from 'react';
 
 function App() {
   const [stories, setStories]= useState([])
   const [error, setError]= useState('')
 
-  // useEffect(() => {
-  //   setError('')
-  //   apiCalls.fetchStoriesData()
-  //   .then((data => setStories(data.results)))
-  //   .catch((err) => console.log(err))
-  // })
+  useEffect(() => {
+    setError('')
+    apiCalls.fetchStoriesData()
+    .then((data => setStories(data.results)))
+    .catch((err) => console.log(err))
+  })
 
   return (
     <div className="App">
-      <Header></Header>
+      <Header/>
+      <StoryCardsArea stories={stories}/>
     </div>
   );
 }
