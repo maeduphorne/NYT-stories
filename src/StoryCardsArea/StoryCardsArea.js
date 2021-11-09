be84e46aa038e1ec270b32084e0eec04110ce6af
Mae Duphorne <maelduphorne@gmail.com>
2021-11-08 18:33:02 -0700

    display story cards

--- src/StoryCardsArea/StoryCardsArea.js
+++ src/StoryCardsArea/StoryCardsArea.js
@@ -1,9 +1,21 @@
 import './StoryCardsArea.css'
 import StoryTitleCard from '../StoryTitleCard/StoryTitleCard';
+import { uniqueId } from 'lodash';
 
-const StoryCardsArea = () => {
+const StoryCardsArea = ({ stories }) => {
+  const storyCards = stories.map((story => {
+    // story Image should map through multimedia within the found story and find the thumbnail sized image
+    const storyImage = story.multimedia.find(image => image.format === "mediumThreeByTwo210")
+    
+    return(
+      <StoryTitleCard
+        story={story}
+        image={storyImage}
+      />
+    )
+  }))
   return (
-    <div></div>
+    <div className="stories-area">{storyCards}</div>
   )
 }
 
