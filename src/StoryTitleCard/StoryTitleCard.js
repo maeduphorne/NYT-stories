24bec593d66d27ac87f0f3326f41414f2d8ed4eb
Mae Duphorne <maelduphorne@gmail.com>
2021-11-08 18:32:45 -0700

    update props name

--- src/StoryTitleCard/StoryTitleCard.js
+++ src/StoryTitleCard/StoryTitleCard.js
@@ -1,15 +1,14 @@
 import './StoryTitleCard.css'
 
-const StoryTitleCard = ({ story, storyImage }) => {
-  // story Image should map through multimedia within the found story and find the thumbnail sized image
+const StoryTitleCard = ({ story, image }) => {
   return(
     <article class="storyTitleCard">
       <img
         className="story-image"
-        src={storyImage.url}
-        alt={storyImage.caption}
+        src={image.url}
+        alt={image.caption}
       />
-      <h3>TITLE: {story.title}</h3>
+      <h3>{story.title}</h3>
     </article>
   )
 }
