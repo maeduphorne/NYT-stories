ed903846ee28ff1c60b929a90ac2ac7ceffee2de
Mae Duphorne <maelduphorne@gmail.com>
2021-11-08 17:23:38 -0700

    comment out fetch to avoid too  many requests

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import apiCalls from './api/apiCalls';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [stories, setStories]= useState([])
-  const [error, setError]= useState('')
+  // const [stories, setStories]= useState([])
+  // const [error, setError]= useState('')
 
-  useEffect(() => {
-    apiCalls.fetchStoriesData()
-    .then((data => setStories(data)))
-    .catch((err) => setError(err))
-  })
+  // useEffect(() => {
+  //   setError('')
+  //   apiCalls.fetchStoriesData()
+  //   .then((data => setStories(data.results)))
+  //   .catch((err) => console.log(err))
+  // })
 
   return (
     <div className="App">
