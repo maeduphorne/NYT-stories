f6a7d537c50f0f36c6d1bb1d53cc1670a50c6bfd
Mae Duphorne <maelduphorne@gmail.com>
2021-11-08 18:03:31 -0700

    import and display header

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,22 @@
 import './App.css';
 import apiCalls from '../api/apiCalls';
+import Header from '../Header/Header';
 import { useEffect, useState } from 'react';
 
 function App() {
   const [stories, setStories]= useState([])
   const [error, setError]= useState('')
 
-  useEffect(() => {
-    setError('')
-    apiCalls.fetchStoriesData()
-    .then((data => setStories(data.results)))
-    .catch((err) => console.log(err))
-  })
+  // useEffect(() => {
+  //   setError('')
+  //   apiCalls.fetchStoriesData()
+  //   .then((data => setStories(data.results)))
+  //   .catch((err) => console.log(err))
+  // })
 
   return (
     <div className="App">
-      Hello World!
+      <Header></Header>
     </div>
   );
 }
