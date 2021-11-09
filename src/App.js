3ae57c25919728d0f9d6463bb58899e742559f57
Mae Duphorne <maelduphorne@gmail.com>
2021-11-08 17:16:05 -0700

    test fetch in app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
+import apiCalls from './api/apiCalls';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [stories, setStories]= useState([])
+  const [error, setError]= useState('')
+
+  useEffect(() => {
+    apiCalls.fetchStoriesData()
+    .then((data => setStories(data)))
+    .catch((err) => setError(err))
+  })
+
   return (
     <div className="App">
       Hello World!
