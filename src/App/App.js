c1ac3b50c0519953fd498d1b2ca304fdbf4f7b9d
Mae Duphorne <maelduphorne@gmail.com>
2021-11-10 08:18:10 -0700

    add storydetaisl for view

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from '../Header/Header';
 import StoryCardsArea from '../StoryCardsArea/StoryCardsArea';
 import StoryDetails from '../StoryDetails/StoryDetails';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <StoryCardsArea />
+      <StoryDetails />
     </div>
   );
 }
