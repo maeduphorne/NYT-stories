8475af430a0df88718942f11266f57a010a176fa
Mae Duphorne <maelduphorne@gmail.com>
2021-11-10 17:14:14 -0700

    add id to storyies in state

--- src/App/App.js
+++ src/App/App.js
@@ -4,18 +4,25 @@ import StoryCardsArea from '../StoryCardsArea/StoryCardsArea';
 import StoryDetails from '../StoryDetails/StoryDetails';
 import apiCalls from '../api/apiCalls';
 import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
   const [stories, setStories]= useState([])
   const [error, setError]= useState('')
 
+  const addID = (data) => {
+    return data.map((story) => ({ ...story, id: uuidv4() }));
+  };
+
   useEffect(() => {
-    setError('')
     apiCalls.fetchStoriesData()
-    .then((data => setStories(data.results)))
+    .then((data => setStories(addID(data.results))))
     .catch((err) => setError(err))
   }, [])
+
+// how to select a single story?
+// on click, find the story that is the target of the click (by ID?)
 
   return (
     <div className="App">
